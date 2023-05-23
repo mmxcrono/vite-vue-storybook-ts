e8c0d162f7af3780454e35afc76d7bff3f0574a2
Hoang <hnguyen.email@gmail.com>
2023-05-23 16:05:22 -0700

    Update Counter example

---
+++ src/components/Counter.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { useCounterStore } from '../stores/counter';
+defineProps<{ msg: string }>();
+const counter = useCounterStore();
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button class="card__button" type="button" @click="counter.increment()">increase</button>
+    <p>{{ counter.count }}</p>
+    <button class="card__button" type="button" @click="counter.decrement()">decrease</button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+
+  /* BEM naming convention */
+  &__button {
+    border: 2px solid transparent;
+  }
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import VueDOMPurifyHTML from 'vue-dompurify-html';
+import { createPinia } from 'pinia';
 
+const pinia = createPinia();
 const app = createApp(App);
 
+app.use(pinia);
 // <template>
 //     <div v-dompurify-html="rawHtml"></div>
     // <div v-dompurify-html:svg="svgContent"></div>
