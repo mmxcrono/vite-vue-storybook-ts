2556018ba231a0afb385137d685cf1a9b0af0abf
Hoang <hnguyen.email@gmail.com>
2023-05-26 17:59:15 -0700

    Minor test update

--- src/components/CounterExample.spec.ts
+++ src/components/CounterExample.spec.ts
@@ -1,13 +1,14 @@
-import { mount } from '@vue/test-utils'
-import CounterExample from './CounterExample.vue'
-import { createTestingPinia } from '@pinia/testing'
-import { useCounterStore } from '../stores/counter'
-
+import { mount } from '@vue/test-utils';
+import CounterExample from './CounterExample.vue';
+import { createTestingPinia } from '@pinia/testing';
+import { useCounterStore } from '../stores/counter';
 
 test('renders CounterExample', () => {
+  const msg = 'My Message';
+
   const wrapper = mount(CounterExample, {
     props: {
-      msg: 'My Message'
+      msg,
     },
     global: {
       plugins: [
@@ -23,10 +24,12 @@ test('renders CounterExample', () => {
   });
 
   const counterStore = useCounterStore();
-  expect(wrapper.text()).toContain('My Message');
+  expect(wrapper.text()).toContain(msg);
   expect(wrapper.text()).toContain('increase');
   expect(wrapper.text()).toContain('0');
   expect(wrapper.text()).toContain('decrease');
   wrapper.find('.card__button-increase').trigger('click');
-  expect(counterStore.increment).toHaveBeenCalledTimes(1)
-})
+  expect(counterStore.increment).toHaveBeenCalledTimes(1);
+  wrapper.find('.card__button-decrease').trigger('click');
+  expect(counterStore.decrement).toHaveBeenCalledTimes(1);
+});
